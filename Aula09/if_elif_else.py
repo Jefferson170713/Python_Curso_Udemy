554d6d6530b63585031def49922a77dcfc10bf3b
Jefferson170713 <jefferson.almeida@aluno.ifce.edu.br>
2022-05-26 23:41:01 -0300

    if, elif e else: estrutura simpificada

---
+++ Aula09/if_elif_else.py
@@ -0,0 +1,24 @@
+"""
+    ********************** [J] ***********************
+    Nome: Jefferson De Almeida            Eng.Produção
+    **************************************************
+"""
+"""
+    IF, ELIF ... .... ELIF, ELSE:
+    Quando o programa encontra uma cnconttra uma condição verdadeira (True) ele executa!
+"""
+# basta que eu coloque True em qualquer condição para ser verdadera!
+nome = 'Jefferson'
+if False:
+
+    print(f"Meu nome é {nome}, estou aprendendo Python."
+          f" E essa instrução 1 é VERDADEIRA!")
+elif False:
+    print(f"Meu nome é {nome}, estou aprendendo Python."
+          f" E essa instrução 2 é VERDADEIRA!")
+elif False:
+    print(f"Meu nome é {nome}, estou aprendendo Python."
+          f" E essa instrução 3 é VERDADEIRA!")
+else:
+    print(f"Meu nome é {nome}, estou aprendendo Python."
+         f" E essa instrução 4 é VERDADEIRA!")
