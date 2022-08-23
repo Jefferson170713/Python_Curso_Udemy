4c0c6e53c95d7bb3e65fe7cf8c160cec2dbc2bb5
Jefferson_de_AG <jefferson.almeida05@aluno.ifce.edu.br>
2022-08-23 19:39:55 -0300

    Com a função reduce(), aprendi a puxar e acumular dados de uma lista pre determinada.

---
+++ Aula25/func_REDUCE_01.py
@@ -0,0 +1,11 @@
+# [J]efferson , Eng. De Produção
+from functools import reduce
+from dados_map import lista_numero, produtos
+soma = reduce(lambda acumulador, item: acumulador + item, lista_numero, 0)
+
+print(soma)
+
+# para deixa claro que o a função reduce faz, ela acumula valores em determinados intervalos.
+# você passa o acumulador, depois aalgo que recebe o valor de algo que queira somar, após isso,
+# você diz que a função lambda deve retornar pra você, depois a lista de onde quer extrair os dados e em seguida
+# de qual situação começar, se é do zero ou nao!
