40f532049672bf5a71bdb45f981fa62c026d18b4
Jefferson_de_AG <jefferson.almeida05@aluno.ifce.edu.br>
2022-08-23 12:43:54 -0300

    teste de git, pois estava dando falha. tentando subir novamente os programas

--- Aula25/dados_map.py
+++ Aula25/dados_map.py
@@ -29,3 +29,31 @@ produtos = [
     {'nome': 'produto 8', 'preço': 25}
 ]
 lista_numero = [1, 2, 3, 4, 5, 6, 7, 8]
+# ********************************************************************
+# [J]efferson , Eng. De Produção
+from dados_map import lista_numero
+
+nova_lista = [lambda x : x > 4 for x in lista_numero] # se eu fizer só assim, retorna um iterador, por isso tentaremos
+# com FILTER
+new_lista = filter(lambda x : x > 4, lista_numero) # aqui eu passo uma função e um dado que eu quero movimentar.
+# parecida com a função map
+
+#print(nova_lista)
+#a = list(nova_lista)
+
+b = list(new_lista)
+for i in b:
+    print(i)
+#for i in nova_lista:
+#   print(i)
+# ********************************************************************
+# [J]efferson , Eng. De Produção
+from dados_map import produtos
+
+prod = filter(lambda p : p['preço'] > 15, produtos)
+
+for i in prod:
+    print(i)
+#   Com essa função filter, consigo passar uma função e uma lista
+# a adiferença é que tem uma condição embutida junto.
+#
