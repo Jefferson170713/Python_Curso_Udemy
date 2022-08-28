917827652ebaa6c3df0e736da90f5ecb93652f62
Jefferson_de_AG <jefferson.almeida05@aluno.ifce.edu.br>
2022-08-28 15:11:00 -0300

    Try excep na aula 26

---
+++ Aula26/Try_except01.py
@@ -0,0 +1,9 @@
+# [J]efferson , Eng. De Produção
+try :
+    print(a/0)
+except Exception:
+    print('Erro, mas o programa continnua...')
+
+# funciona como se fosse um if e else, mas trata os erros do código
+# este nome Exception na except serve para tratar uma um erro qualquer.
+# https://docs.python.org/3/library/exceptions.html ----> documentação.
