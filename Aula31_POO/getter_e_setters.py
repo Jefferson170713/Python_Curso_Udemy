0b9d4bea2899c7b4cf3dd320880327d278885438
Jefferson DataScience <jefferson.almeida05@aluno.ifce.edu.br>
2022-10-11 13:57:16 -0300

    Aprendendo sobre getter e setters, que funcionam basicamente como filtros no python, sem a necessidade de mexer no código original quando ouver erro de digitação por parte do usuário, como pasasr uma str ao inves de int ou float.

---
+++ Aula31_POO/getter_e_setters.py
@@ -0,0 +1,34 @@
+# [J]efferson , Eng. De Produção
+class Produto:
+
+    def __init__(self, nome, valor):
+        self.nome = nome
+        self.valor = valor
+
+    def Desconto(self, percentual):
+        self.valor = self.valor - (self.valor * (percentual/100))
+
+    #Getter
+    @property
+    def valor(self): # Aqui eu gero o getter ou obtenho o valor, repare qe o nome da variável mudou um pouco.
+        return self._valor # ficou este nome para colocar no setter
+
+    @valor.setter
+    def valor(self, preco): # Aqui é o setter, com isso, repare que é o mesmo nome da variável, também temos uma função que verifica se é str
+        if isinstance(preco, str):
+            preco = float(preco.replace("R$", ""))# Aqui troca o R$ po nada e ao mesmo tempo que faz o casting para float.
+        self._valor = preco
+        return self._valor
+
+
+prod = Produto('Camisa', 45)
+
+prod.Desconto(25)
+print(prod.valor) # Como podemos ver, passei o nome e valor da classe e em seguida fiz o calculo
+
+# Agora veremos passando de outra forma, com uma str
+prod2 = Produto('Calça', '60 R$')
+prod2.Desconto(25)
+
+print(prod2.valor) # Agora o código vai funcionar perfeitamente, mesmo passando str.
+
