1e247e092430a5d2aeb5157eacab83db80d48d32
Jefferson DataScience <jefferson.almeida05@aluno.ifce.edu.br>
2023-01-14 14:09:09 -0300

    Prêmio lotofácil, programa para conferir.

---
+++ Programas e atividades/Conferir Prêmios/Conferir lotofácil..py
@@ -0,0 +1,7 @@
+# [J]efferson , Eng. De Produção
+
+premio = [2, 3, 5, 6, 7, 8, 9, 12, 15, 18, 19, 20, 21, 22, 24]
+jogada = [2, 3, 4, 6, 9, 10, 12, 15,]
+
+for _, x in enumerate(premio):
+    print(_ + 1, x)
