e443bcc170a3b0365a22eb08a0cf905575d40250
Jefferson DataScience <jefferson.almeida05@aluno.ifce.edu.br>
2023-01-26 20:46:32 -0300

    Trabalhando e estudando detecção de imagens no python.

--- Programas e atividades/Conferir Prêmios/Conferir lotofácil..py
+++ Programas e atividades/Conferir Prêmios/Conferir lotofácil..py
@@ -1,7 +1,22 @@
 # [J]efferson , Eng. De Produção
 
 premio = [2, 3, 5, 6, 7, 8, 9, 12, 15, 18, 19, 20, 21, 22, 24]
-jogada = [2, 3, 4, 6, 9, 10, 12, 15,]
+jogada1 = [2, 3, 4, 6, 9, 10, 12, 15, 16, 18, 19, 21, 22, 23, 24]
+jogada2 = [1, 2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22, 24, 25]
+qtd = 0
+#print(len(jogada) == len(premio))
+a = [1, 2, 3]
+b = [2, 3, 4]
 
-for _, x in enumerate(premio):
-    print(_ + 1, x)
+print(len(jogada2) == len(premio))
+
+
+
+for num, x in enumerate(jogada1):
+    for y in premio:
+        if x == y:
+            print(x, y)
+            qtd +=1
+
+
+print(f'Acertos : {qtd}')
