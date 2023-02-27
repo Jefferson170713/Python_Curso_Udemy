1c1c1e9bfb054d097603a6f9c7c470e47ccb36f0
Jefferson DataScience <jefferson.almeida05@aluno.ifce.edu.br>
2023-02-27 13:37:33 -0300

    trabalhando os novos coonhecimentos em *args do python junto com o POO

--- Aula_38_game/main.py
+++ Aula_38_game/main.py
@@ -7,6 +7,15 @@ p2 = PessoaHabilidades('Rayssa', 26, 67.00, 'Feminino', 'Números')
 p3 = PessoaHabilidades('Jonas', 28, 102.85, 'Masculino', 'Estudos')
 p4 = PessoaHabilidades('Herverton', 25, 71.45, 'Masculino', 'Sofrer pelo Vasco')
 p5 = PessoaHabilidades('Allan', 27, 95.78, 'Masculino', 'Guardar Rancor')
+# nova pessoa adicionada na lista
+p6 = PessoaHabilidades('Fagner Matos', 27, 71.42, 'Masculino', 'Tocar Teclado')
+
+
 #print(Pessoa.lista_pessoas)
+for n, p in enumerate(Pessoa.lista_pessoas):
+    print(f'{n+1} : {p}')
+p6.carreiracurso('Falar Inglês')
+p6.carreiracurso('Habilidade em codar em Java')
+print(100*'*')
 for n, p in enumerate(Pessoa.lista_pessoas):
     print(f'{n+1} : {p}')
--- Aula_38_game/pessoa_aatributos.py
+++ Aula_38_game/pessoa_aatributos.py
@@ -4,9 +4,19 @@ from pessoa_padrao import Pessoa
 class PessoaHabilidades(Pessoa):
     #Lista de Pessoas com habilidades
     lista_habilidade = []
+    lista_curso = []
+
     def __init__(self, nome: str, idade: int, peso: float, sexo: str, habilidade):
         super().__init__(nome, idade, peso, sexo)
         self.habilidade = habilidade
+        self.curso = None
         PessoaHabilidades.lista_habilidade.append(self)
     def __repr__(self):
-        return f'[Nome: {self.nome}, Idade: {self.idade}, Peso: {self.peso} Kg, Sexo: {self.sexo}, Habilidade: {self.habilidade}]'
+        if self.curso == None:
+            return f'[Nome: {self.nome}, Idade: {self.idade}, Peso: {self.peso} Kg, Sexo: {self.sexo}, Habilidade: {self.habilidade}]'
+        else:
+            return f'[Nome: {self.nome}, Idade: {self.idade}, Peso: {self.peso} Kg, Sexo: {self.sexo}, Habilidade: {self.habilidade}, Cursos: {self.curso}]'
+    def carreiracurso(self, *args):
+        PessoaHabilidades.lista_curso.append(args)
+        self.curso = PessoaHabilidades.lista_curso
+        return self.curso
